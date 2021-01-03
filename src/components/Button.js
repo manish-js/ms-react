ecca1b8bbfe76ee24a4927300c2943bcf180594f
Manish Sharma <Manish.Sharma2@Staples.com>
2021-01-03 16:41:02 +0530

    First Component in react

---
+++ src/components/Button.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+const Button = () => {
+    const handleClick = () => console.log('Button Clicked!');
+    return <button onClick={handleClick}>Click Me! </button>;
+}
+
+export default Button;
